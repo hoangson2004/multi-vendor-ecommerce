aaa771ff4f2e8a37ef3db42320d68260c83a1f30
hoangson2004 <lehoangson022004@gmail.com>
2025-08-26 14:31:11 +0700

    refactor package

---
+++ product-service/src/main/java/hust/hoangson/product/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package hust.hoangson.product.exception;
+
+import hust.hoangson.product.response.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<BaseResponse<Object>> handleRuntimeException(RuntimeException ex) {
+        logger.error(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(BaseResponse.error(400, ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<Object>> handleValidationException(MethodArgumentNotValidException ex) {
+        logger.error(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(BaseResponse.error(400,  ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponse<Object>> handleGenericException(Exception ex) {
+        logger.error(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(BaseResponse.error(500, "Internal Server Error"));
+    }
+}
---
+++ product-service/src/test/java/hust/hoangson/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hust.hoangson.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
