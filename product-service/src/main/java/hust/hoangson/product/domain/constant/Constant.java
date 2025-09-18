9e3e4db8320126e85d2dc20e6bdacaa2a97c22fc
hoangson2004 <lehoangson022004@gmail.com>
2025-09-18 21:45:59 +0700

    add cloudinary-product

---
+++ product-service/src/main/java/hust/hoangson/product/domain/constant/Constant.java
@@ -0,0 +1,13 @@
+package hust.hoangson.product.domain.constant;
+
+public class Constant {
+    public static class Cloudinary {
+        public static final String CLOUD_NAME = "cloud_name";
+        public static final String API_KEY = "api_key";
+        public static final String API_SECRET = "api_secret";
+        public static final String FOLDER = "folder";
+        public static final String CATALOG = "catalog/";
+        public static final String SECURE_URL = "secure_url";
+    }
+
+}
